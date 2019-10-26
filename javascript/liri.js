01e6ec58b5289df5e66ad4117369e4caafdf34b5
chason <chasonlsmith@gmail.com>
2019-10-26 01:07:15 -0600

    Figured out why  movie title, year, value, country, language, plot, and actors wouldn't show up for movieSearch function. added doWhatItSays function for fs.readFile.

--- javascript/liri.js
+++ javascript/liri.js
@@ -57,4 +57,62 @@ function concertSearch(artistName) {
             }
         }
     })
+       .catch(function(error) {
+           console.log("There were no events found for Artist");
+       });
     }
+
+        function spotifySearch(songTitle) {
+            spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+                if (err) {
+                    return console.log('Error: ' + err);
+                }
+                //printing artist, song
+                console.log('Artist Name: ', data.tracks.items[0].album.artists[0].name)
+                console.log("Track Title: ", data.tracks.items[0].name)
+                // if statement to show preview if it doesn't have a preview of the song print that its not available
+                if (data.tracks.items[0].preview_url === null) {
+                    console.log('Preview URL: No preview available for this track')
+                } else {
+                    console.log('Track Preview: ', data.tracks.items[0].preview_url);
+                }
+                // also printing album
+                console.log('Album: ', data.tracks.items[0].album.name)
+            });
+
+        };
+
+        function movieSearch(movieTitle) {
+            var movieURL = "https://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy"
+            axios.get(movieURL)
+            .then(function(response) {
+                //movie title
+                console.log('Movie Title: ', response.data.Title)
+                //movie year released
+                console.log('Year Released: ', response.data.Year)
+                //IMDB movie ratings
+                console.log("IMDB Rating: ", response.data.Ratings[1].Value)
+                //This is where the movie was filmed ie country/location
+                console.log('Country: ', response.data.Country)
+                //Language for Movie
+                console.log('Movie Language: ', response.data.Language)
+                //plot summary
+                console.log("Plot: ", response.data.Plot)
+                //Actors/Actresses
+                console.log('Actors: ', response.data.Actors)
+
+            }).catch(function(error) {
+                console.log(error) // print error for eaach funtion
+
+            });
+        };
+
+            function doWhatItSays() {
+                fs.readFile('random.txt', 'utf8', function(error, data) {
+                    if (error) {
+                        return console.log(error)
+                    }
+                    console.log(data)
+                    spotifySearch(data.split(',')[1])
+                })
+            }
